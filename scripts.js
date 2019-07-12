1a0cd47b5e0a76f7d0c69904ddc85ca8850a3895
Cummings <austin.cummings@audatex.com>
2019-07-12 15:53:36 -0700

    set, needs date modifications

--- scripts.js
+++ scripts.js
@@ -3,7 +3,9 @@ var btn = document.querySelector('#VinResponse');
 //todo make astring
 var genericText = "somethingwitty";
 btn.addEventListener('click', function (event) {
-    alert(Date);
+    var myDate = new Date();
+    console.log(myDate.getDate(),number);
+    // alert(myDate);
 	console.log(event); // The event details
     console.log(event.target); // The clicked element
     console.log(genericText);
