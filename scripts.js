fa643061304a920c12863f5a3f80adda362d8a31
Austin C <austins.accts@gmail.com>
2019-07-15 15:43:22 -0700

    returns digits as 2 placehoolders

--- scripts.js
+++ scripts.js
@@ -1,17 +1,28 @@
-// function readableTime(i) {
-//     if(i<10) 
-// }
+
+//functions below
+//makes single digits into doubles
+function readableTime(num1,num2,num3,num4) {
+    h = num1++;
+    var timeNumber;
+    if(h>12){
+        timeNumber = h - 12;
+    }
+    if (h < 10) {
+        timeNumber = '0' + h;
+    }
+    else {
+        timeNumber = h;
+    }
+    return (timeNumber);
+};
 
 
-
-
-
-
-console.log("scripts.js is linked");
 var btn = document.querySelector('#VinResponse');
 //todo make astring
 var genericText;
 btn.addEventListener('click', function (event) {
+    var test = readableTime(13,1,1,1);
+    alert(test);
     var currentDate = new Date();
     var date = currentDate.getDate();
     var month = currentDate.getMonth(); //Be careful! January is 0 not 1
@@ -20,8 +31,8 @@ btn.addEventListener('click', function (event) {
     var timeStamper = date + "/" +(month) + "/" + year + " " + "this should be hour =>" + hour ;
     // console.log(myDate.getDate(),number);
     // alert(myDate);
-	console.log(event); // The event details
+
+    console.log(event); // The event details
     console.log(event.target); // The clicked element
-    console.log(genericText);
     document.querySelector('#outputField').append(timeStamper);
 }, false);
